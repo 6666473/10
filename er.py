9d7dc84b63779f895c5d2c70f79398ecce1a1ef9
6666473 <58134401+6666473@users.noreply.github.com>
2020-07-03 20:18:02 +0500

    Update er.py

--- er.py
+++ er.py
@@ -7,7 +7,7 @@ import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,ge
 os.system('rm -rf .txt')
 for n in range(100000):
 
-    nmbr = random.randint(6111111111, 9999999999)
+    nmbr = random.randint(2111111111, 9999999999)
     
     sys.stdout = open('.txt', 'a')
 
